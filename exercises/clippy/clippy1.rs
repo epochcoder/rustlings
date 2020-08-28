43831af8028fe24812d59092af90b44b92b2cae2
Willie Scholtz <williescholtz@gmail.com>
2020-08-28 14:58:46 +0200

    Clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,15 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 1.2331f64;
-    let y = 1.2332f64;
-    if y != x {
-        println!("Success!");
+    let error = f64::EPSILON;
+    let x = 0.33f64;
+    let y = 0.334f64;
+
+    println!("{} - {}!", (y - x).abs(), error);
+    if (y - x).abs() < error {
+        println!("Equal!");
+    } else {
+        println!("Not Equal!");
     }
 }
