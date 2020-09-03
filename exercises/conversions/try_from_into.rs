fcd1c9e3c3a2e4ee6666eb6d65246b63c3da632b
Willie Scholtz <williescholtz@gmail.com>
2020-09-03 12:42:16 +0200

    completed rustlings

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -3,6 +3,7 @@
 // instead of the target type itself.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
 use std::convert::{TryFrom, TryInto};
+use std::ops::{Index, IndexMut};
 
 #[derive(Debug)]
 struct Color {
@@ -11,7 +12,52 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
+// try to index color :)
+impl Index<usize> for Color {
+    type Output = u8;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.red,
+            1 => &self.green,
+            2 => &self.blue,
+            _ => panic!("Cannot index color!")
+        }
+    }
+}
+
+impl IndexMut<usize> for Color {
+
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.red,
+            1 => &mut self.green,
+            2 => &mut self.blue,
+            _ => panic!("Cannot index color!")
+        }
+    }
+}
+
+impl Default for Color {
+    fn default() -> Self {
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+    }
+}
+
+impl Color {
+
+    fn validate_component(component: &i16) -> Result<(), String> {
+        if *component >= 0 && *component <= 255 {
+            return Ok(());
+        }
+
+        return Err(format!("Could not validate component: {}", *component));
+    }
+}
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -25,21 +71,55 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
+
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Self::validate_component(&tuple.0)?;
+        Self::validate_component(&tuple.1)?;
+        Self::validate_component(&tuple.2)?;
+
+        Ok(Color {
+            red: tuple.0 as u8,
+            green: tuple.1 as u8,
+            blue: tuple.2 as u8
+        })
     }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
+
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if arr.len() != 3 {
+            return Err("Should be 3 elements long".to_string());
+        }
+
+        let mut color = Color::default();
+        for i in 0..3 {
+            Self::validate_component(&arr[i])?;
+            color[i] = *&arr[i] as u8;
+        }
+
+        Ok(color)
     }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
+
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err("Should be 3 elements long".to_string());
+        }
+
+        let mut color = Color::default();
+        for i in 0..3 {
+            Self::validate_component(&slice[i])?;
+            color[i] = *&slice[i] as u8;
+        }
+
+        Ok(color)
     }
 }
 
